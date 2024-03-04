bb38dca9ae41074d0edc6e1a54f85fcc2658173b
Aniket <aniketjha680@gmail.com>
2024-03-05 02:49:53 +0530

    Added gallery and footer

--- script.js
+++ script.js
@@ -40,3 +40,18 @@ document.addEventListener('DOMContentLoaded', function(event) {
     // Start the text animation
     StartTextAnimation(0);
 });
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("myslides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
